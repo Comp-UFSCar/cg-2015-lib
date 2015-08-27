fe22d896b08d400e728b0ca994b1241485858374
thamenato <thamenato@gmail.com>
2015-08-27 16:15:21 -0300

    Added debug folder

---
+++ debug/monitor_debug.c
@@ -0,0 +1,20 @@
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <X11/Xlib.h>
+#include <X11/Xutil.h>
+
+#include "../lib/structs_lib.h"
+#include "../lib/functions_lib.h"
+
+int main(int argc, char *argv[]) {
+    printf("##############\tMonitor Debug\t##############\n");
+
+    struct Point p0, p1;
+    struct Image img;
+
+
+
+    return 0;
+}
