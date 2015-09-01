3b3ae0bcc0b20142f18234720ebaaf428ca7b871
thamenato <thamenato@gmail.com>
2015-09-01 14:43:11 -0300

    Debug to verify if hotfix is working.

--- debug/monitor_debug.c
+++ debug/monitor_debug.c
@@ -3,10 +3,27 @@
 int main(int argc, char *argv[]) {
     printf("##############\tMonitor Debug\t##############\n");
 
-    struct Point    p0u = {3, 5}, p0n, p0d,
-                    p1u = {15, 15}, p1n, p1d;
+    struct Point    p0u = {-50, -50}, p0n, p0d,
+                    p1u = {50, 50}, p1n, p1d;
     struct Image img;
 
+    init_image(&img);
+    print_image(img);
+
+    p0d.x = 1;
+    p0d.y = 1;
+    p1d.x = 15;
+    p1d.y = 10;
+
+    img = drawLineDDA(p0d, p1d);
+
+    printf("\n Depois \n");
+    print_image(img);
+
+    printf("\n\n############## DDA SRU ##############\n");
+
+    init_image(&img);
+    print_image(img);
 
     // SRU 2 SRN
     printf("SRU 2 SRN\n");
@@ -25,13 +42,9 @@ int main(int argc, char *argv[]) {
     printf("x: %lf y: %lf\n", p0d.x, p0d.y);
     printf("x: %lf y: %lf\n", p1d.x, p1d.y);
 
-//    init_image(&img);
-//    print_image(img);
+    img = drawLineDDA(p0d, p1d);
+    print_image(img);
 
-    img = drawLine_Bresenham(p0d, p1d);
-
-//    printf("\n Depois \n");
-//    print_image(img);
 
     return 0;
 }
