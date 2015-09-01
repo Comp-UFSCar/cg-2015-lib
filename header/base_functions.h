86c4f37999bf56f86f449c6afbf621314f62d113
thamenato <thamenato@gmail.com>
2015-09-01 14:42:50 -0300

    Hotfix for define on function name
    Changed Bresenhain to DDA.

--- header/base_functions.h
+++ header/base_functions.h
@@ -10,10 +10,10 @@
 void XDump(XImage *ximage, struct Image img);
 struct Point sru2srn(struct Point p, double xmin, double ymin, double xmax, double ymax);
 struct Point srn2srd(struct Point mc);
-struct Image drawLine_Bresenham(struct Point p0, struct Point p1);
+struct Image drawLineDDA(struct Point p0, struct Point p1);
 void init_image(struct Image *img);
 void print_image(struct Image img);
-struct Image input_line_bresenham(char *argv[]);
-struct Image input_line_bresenham_sru(char *argv[]);
+struct Image inputLineDDA(char **argv);
+struct Image inputLineDDA_sru(char **argv);
 
 #endif
--- header/console_input.h
+++ header/console_input.h
@@ -6,21 +6,21 @@
 
 #define INPUT_ERROR -2
 #define INPUT_MISTAKE -1
-#define LINE_BRESENHAM 0
-#define LINE_BRESENHAM_SRU 1
+#define LINE_DDA 0
+#define LINE_DDA_SRU 1
 
-#define LINE_BRESENHAM_INCORRECT "\nIncorrect usage. To use '-line_bresenham' you must follow:\n" \
-                                "\t-line_bresenham x0 y0 x1 y1\n" \
+#define MSG_LINE_DDA_INCORRECT "\nIncorrect usage. To use '-line_dda' you must follow:\n" \
+                                "\t-line_dda x0 y0 x1 y1\n" \
                                 "All numbers must be integers\n\n"
 
-#define LINE_BRESENHAM_SRU_INCORRECT "\nIncorrect usage. To use '-line_bresenham -sru' you must follow:\n" \
-                                    "\t-line_bresenham -sru x0 y0 x1 y1 xmin ymin xmax ymax\n" \
+#define MSG_LINE_DDA_SRU_INCORRECT "\nIncorrect usage. To use '-line_dda -sru' you must follow:\n" \
+                                    "\t-line_dda -sru x0 y0 x1 y1 xmin ymin xmax ymax\n" \
                                     "Real number can be used.\n\n"
 
 #define HELP_MENU "Usage: monitor_cthulhu [options]\n" \
                     "Options:\n" \
-                    " -line_bresenham [-sru] <param>\n" \
-                    "\t\t\t\tDraw a line using Bresenham's algorithm.\n" \
+                    " -line_dda [-sru] <param>\n" \
+                    "\t\t\t\tDraw a line using DDA's algorithm.\n" \
                     "\t\t\t\tParameter are all integers: 'x0 y0 x1 y1'\n" \
                     "\t\t\t\tThe '-sru' option receives points that\n" \
                     "\t\t\t\tare using the SRU, the parameters are\n" \
